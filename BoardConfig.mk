4b52c1fa2785f3b82ae659f619b07ed9ef982c66
Rokib Hasan Sagar <rokibhasansagar2014@outlook.com>
2020-11-04 17:14:55 +0600

    5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,7 @@ MTK_HARDWARE := true
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a53
